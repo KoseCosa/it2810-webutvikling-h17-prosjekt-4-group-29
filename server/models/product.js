81e2f3bf8982e21e7778dd492289df67bb83a07b
MrHanSan <hans.sandbu@protonmail.com>
2017-11-13 17:19:37 +0100

    added a line that was wrongfully removed

--- server/models/product.js
+++ server/models/product.js
@@ -1,5 +1,6 @@
 const Int32 = require('mongoose-int32');
 const mongoose = require('mongoose');
+require('mongoose-double')(mongoose);
 const Schema = mongoose.Schema;
 const SchemaTypes = mongoose.Schema.Types;
 

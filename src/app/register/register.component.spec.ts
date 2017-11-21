a196e841410b884b516304d6aa951cebeb53588e
Ole Jakob Schjøth <olejsc@stud.ntnu.no>
2017-11-21 16:24:30 +0100

    Added tests for the register component.

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,5 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import {RouterTestingModule} from '@angular/router/testing';
+
+import { AuthService } from '../auth.service';
+import { ValidateService } from '../validate.service';
+
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
@@ -8,7 +16,16 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      providers: [
+        AuthService,
+        ValidateService
+      ],
+      imports : [
+        FormsModule,
+        HttpModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));

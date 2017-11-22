e5ed115b51fcfb7b52a89a34f1de72422cf35ba9
olejsc <olejsc@ntnu.no>
2017-11-22 20:51:46 +0100

    fixed indentation + added visual studio code folder to gitignore

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -7,83 +7,83 @@ import { BaseRequestOptions, Http, RequestMethod, Response, ResponseOptions } fr
 
 describe('DataService', () => {
 
-    let subject: DataService;
-    let backend: MockBackend;
+  let subject: DataService;
+  let backend: MockBackend;
 
-    beforeEach(() => {
+  beforeEach(() => {
 
-        TestBed.configureTestingModule({
-            providers: [
-                MockBackend,
-                BaseRequestOptions,
-                {
-                    provide: Http,
-                    useFactory: (backendInstance: MockBackend, defaultOptions: BaseRequestOptions) => {
-                        return new Http(backendInstance, defaultOptions);
-                    },
-                    deps: [MockBackend, BaseRequestOptions]
-                },
-                DataService
-            ]
-        });
+    TestBed.configureTestingModule({
+      providers: [
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backendInstance: MockBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backendInstance, defaultOptions);
+            },
+          deps: [MockBackend, BaseRequestOptions]
+          },
+          DataService
+      ]
     });
+  });
 
-    beforeEach(inject([DataService, MockBackend], (DataService: DataService, mockBackend: MockBackend) => {
-        subject = DataService;
-        backend = mockBackend;
-    }));
+  beforeEach(inject([DataService, MockBackend], (DataService: DataService, mockBackend: MockBackend) => {
+      subject = DataService;
+      backend = mockBackend;
+  }));
 
-    it('#Dataservice should get success in return', (done) => {
-        backend.connections.subscribe((connection: MockConnection) => {
-            const options = new ResponseOptions({
-                body: '{"success": "true"}',
-                status: 200
-            });
-            connection.mockRespond(new Response(options));
-
-            // Check the request method
-            expect(connection.request.method).toEqual(RequestMethod.Get);
-            // Check the url
-            expect(connection.request.url).toEqual('http://localhost:8084/api/users');
-            // Check the body
-            // Check the request headers
-            expect(connection.request.headers.get('Content-Type')).toEqual('application/json');
+  it('#Dataservice should get success in return', (done) => {
+    backend.connections.subscribe((connection: MockConnection) => {
+      const options = new ResponseOptions({
+        body: '{"success": "true"}',
+        status: 200
         });
+      connection.mockRespond(new Response(options));
 
-        subject
-            .getUsers()
-            .subscribe((response) => {
-                // Check the response
-                expect(response.success).toBe('true');
-                done();
-            });
+      // Check the request method
+      expect(connection.request.method).toEqual(RequestMethod.Get);
+      // Check the url
+      expect(connection.request.url).toEqual('http://localhost:8084/api/users');
+      // Check the body
+      // Check the request headers
+      expect(connection.request.headers.get('Content-Type')).toEqual('application/json');
     });
-
-    it('#Dataservice should get products in return', (done) => {
-      backend.connections.subscribe((connection: MockConnection) => {
-        const options = new ResponseOptions({
-            body: '{"success": "true"}',
-            status: 200
-        });
-        connection.mockRespond(new Response(options));
-
-        // Check the request method
-        expect(connection.request.method).toEqual(RequestMethod.Get);
-        // Check the url
-        expect(connection.request.url).toEqual('http://localhost:8084/api/products?search=%22Vodka%22');
-        // Check the body
-        // Check the request headers
-        expect(connection.request.headers.get('Content-Type')).toEqual('application/json');
-      });
 
     subject
-        .getProducts('Vodka')
-        .subscribe((response) => {
-            // Check the response
-            expect(response.success).toBe('true');
-            done();
-        });
+      .getUsers()
+      .subscribe((response) => {
+      // Check the response
+        expect(response.success).toBe('true');
+        done();
+      });
+  });
+
+  it('#Dataservice should get products in return', (done) => {
+    backend.connections.subscribe((connection: MockConnection) => {
+      const options = new ResponseOptions({
+        body: '{"success": "true"}',
+        status: 200
+      });
+      connection.mockRespond(new Response(options));
+
+      // Check the request method
+      expect(connection.request.method).toEqual(RequestMethod.Get);
+      // Check the url
+      expect(connection.request.url).toEqual('http://localhost:8084/api/products?search=%22Vodka%22');
+      // Check the body
+      // Check the request headers
+      expect(connection.request.headers.get('Content-Type')).toEqual('application/json');
     });
+
+    subject
+      .getProducts('Vodka')
+      .subscribe((response) => {
+      // Check the response
+        expect(response.success).toBe('true');
+        done();
+      });
+  });
 });
 
 

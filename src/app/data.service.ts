0ccf46ec905489abce2479e6587d819d740bc7ee
MrHanSan <hans.sandbu@protonmail.com>
2017-11-22 13:35:38 +0100

    Added data folder to gitignore, and changed port to 8084

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,7 +11,7 @@ export class DataService {
 
     headers.append('Content-Type', 'application/json');
 
-    return this._http.get('http://localhost:3000/api/countries', {headers: headers})
+    return this._http.get('http://localhost:8084/api/countries', {headers: headers})
       .map(res => res.json());
   }
   getUsers() {
@@ -19,7 +19,7 @@ export class DataService {
 
     headers.append('Content-Type', 'application/json');
 
-    return this._http.get('http://localhost:3000/api/users', {headers: headers})
+    return this._http.get('http://localhost:8084/api/users', {headers: headers})
       .map(res => res.json());
   }
 
@@ -30,7 +30,7 @@ export class DataService {
     headers.append('Content-Type', 'application/json');
     params.set('search', JSON.stringify(search));
 
-    return this._http.get('http://localhost:3000/api/products', {headers: headers, params: params})
+    return this._http.get('http://localhost:8084/api/products', {headers: headers, params: params})
       .map(res => res.json());
   }
 
@@ -39,7 +39,7 @@ export class DataService {
 
     headers.append('Content-Type', 'application/json');
 
-    return this._http.get('http://localhost:3000/api/producttypes', {headers: headers})
+    return this._http.get('http://localhost:8084/api/producttypes', {headers: headers})
       .map(res => res.json());
   }
 
@@ -49,7 +49,7 @@ export class DataService {
     headers.append('Content-Type', 'application/json');
     const params = {idList: idList};
 
-    return this._http.get('http://localhost:3000/api/productsById', {headers: headers, params: params})
+    return this._http.get('http://localhost:8084/api/productsById', {headers: headers, params: params})
       .map(res => res.json());
   }
 
@@ -58,7 +58,7 @@ export class DataService {
     headers.append('Content-Type', 'application/json');
     const params = {user: userID};
 
-    return this._http.get('http://localhost:3000/api/userFavorites', {headers: headers, params: params})
+    return this._http.get('http://localhost:8084/api/userFavorites', {headers: headers, params: params})
       .map(res => res.json());
   }
 
@@ -67,7 +67,7 @@ export class DataService {
     headers.append('Content-Type', 'application/json');
     const params = {user: userID};
 
-    return this._http.get('http://localhost:3000/api/userFavoriteObjects', {headers: headers, params: params})
+    return this._http.get('http://localhost:8084/api/userFavoriteObjects', {headers: headers, params: params})
       .map(res => res.json());
   }
 
@@ -75,7 +75,7 @@ export class DataService {
   updateRemoteUser(updateValues) {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    this._http.post('http://localhost:3000/api/addFavorites', updateValues,
+    this._http.post('http://localhost:8084/api/addFavorites', updateValues,
       {headers: headers, withCredentials: true}).subscribe();
   }
 
@@ -83,13 +83,13 @@ export class DataService {
     const headers = new Headers();
     const params = {updateValues: updateValues};
     headers.append('Content-Type', 'application/json');
-    return this._http.get('http://localhost:3000/api/removeFavorite',
+    return this._http.get('http://localhost:8084/api/removeFavorite',
       {headers: headers, params: params, withCredentials: true});
   }
   getSpecificProduct() {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this._http.get('http://localhost:3000/api/specificProducts', {headers: headers})
+    return this._http.get('http://localhost:8084/api/specificProducts', {headers: headers})
       .map(res => res.json());
   }
 
@@ -100,7 +100,7 @@ export class DataService {
     headers.append('Content-Type', 'application/json');
     params.set('search', JSON.stringify(search));
 
-    return this._http.get('http://localhost:3000/api/autocomplete', {headers: headers, params: params})
+    return this._http.get('http://localhost:8084/api/autocomplete', {headers: headers, params: params})
       .map(res => res.json());
   }
 }

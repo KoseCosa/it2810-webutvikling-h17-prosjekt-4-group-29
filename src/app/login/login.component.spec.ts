ce93f313020a532a2c899e4a1427c0f8b01f01fa
olejsc <olejsc@ntnu.no>
2017-11-22 15:31:33 +0100

    Added simple & working test to login component. Nav-bar component test does not work atm.

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,5 +1,18 @@
+// Angular testing modules & classes
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+// Essential angular needed
+import { By } from '@angular/platform-browser';
+import { FormsModule, NgModel } from '@angular/forms';
+import { NgModule, DebugElement } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+// Other componets/services, neded for test to work for the targetted specific test subject
+import { AuthService } from '../auth.service';
+import { ValidateService } from '../validate.service';
+
+// Targetted test subject - a service or a component
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,7 +21,16 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      providers: [
+        AuthService,
+        ValidateService
+      ],
+      imports : [
+        FormsModule,
+        HttpModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,5 +1,22 @@
+// Angular testing modules & classes
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+// Essential angular needed
+import { By } from '@angular/platform-browser';
+import { FormsModule, NgModel } from '@angular/forms';
+import { NgModule, DebugElement } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { Router, RouterModule } from '@angular/router';
+import { CollapseModule } from 'ngx-bootstrap/collapse/collapse.module';
+
+// Other componets/services, neded for test to work for the targetted specific test subject
+import { AuthService } from '../auth.service';
+import { ValidateService } from '../validate.service';
+import { NavSearchService } from '../nav-search.service';
+import { DataService } from '../data.service';
+
+// Targetted test subject - a service or a component
 import { NavBarComponent } from './nav-bar.component';
 
 describe('NavBarComponent', () => {
@@ -8,7 +25,20 @@ describe('NavBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavBarComponent ]
+      declarations: [ NavBarComponent ],
+      providers: [
+        AuthService,
+        ValidateService,
+        NavSearchService,
+        DataService
+      ],
+      imports : [
+        FormsModule,
+        HttpModule,
+        RouterTestingModule,
+        RouterModule,
+        CollapseModule
+      ]
     })
     .compileComponents();
   }));
